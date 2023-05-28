626d8f23673e5beab6815604083989aa82e271f1
cecil <82148488+cecvl@users.noreply.github.com>
2023-05-28 23:16:11 +0300

    compund interest formula(annually)

---
+++ compound_interest.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h> // to allow use of pow() function
+main(){
+    
+    float pri, rate, time, ci, ratetime, calcrate;
+    
+    printf("Enter the principle amount, rate, time(in years):");
+    scanf("%f %f %f",& pri, & rate, & time);
+
+    //break down the formula 
+    calcrate = 1 + (rate/100);
+    ratetime = pow(calcrate, time);  //pow() function calculates the power of any given number
+    ci = pri * ratetime;
+
+    printf("Your total amount = %f ", ci) ;
+}

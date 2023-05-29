a13807694d219736fc471a371d6f6a9f0fb06d0f
cecil <82148488+cecvl@users.noreply.github.com>
2023-05-29 15:56:59 +0300

    grammar

--- simple_interest.c
+++ simple_interest.c
@@ -2,7 +2,7 @@
 main(){
 
     float pri, rate, time, si;  //use float  to allow inputs in decimals too
-    printf("Good evening.Enter principle amount, rate, time:");
+    printf("Enter principle amount, rate, time:");
     scanf("%f %f %f", & pri, & rate, & time);
 
     si = (pri * rate * time)/100; //division by 100 because rates are in percentages

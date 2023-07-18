1d1b04bc9c4f75cbf7e8d961e07090d2545d5e19
Cecil <cecioasuna@gmail.com>
2023-07-18 15:44:51 +0300

    adding constant PI

--- area_and_perimeter_circle.c
+++ area_and_perimeter_circle.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <math.h>
 
+#define PI 3.14285714
 
-main(){
+int main()
+{
 
     float radius, area, perimeter;
-    const double pi =  3.14285714; //constant
+    //const double pi =  3.14285714; 
 
     printf("Enter the radius:");
     scanf("%f", & radius);
 
-    area = pi * radius * radius;  // pi by radius squared
-    perimeter = pi * radius * 2;  // pi by diameter
+    area = PI * radius * radius;  // pi by radius squared
+    perimeter = PI * radius * 2;  // pi by diameter
 
     printf(" The area is : %f", area);
-
 
     printf("\n The perimeter is : %f", perimeter);
 
